afb6ffce3ee4e61c958f8d3b9fbc5ce408495eab
Martyn <zogacc@gmail.com>
2022-03-17 15:49:00 +0200

    feat(ui): add request status toasts

---
+++ src/shared/constants.ts
@@ -0,0 +1,6 @@
+export const REQUEST_STATUS_TO_TYPE_MAP = {
+  IDLE: "secondary" as const,
+  REQUEST: "secondary" as const,
+  SUCCESS: "success" as const,
+  FAILURE: "warning" as const,
+};
--- src/shared/utils/utils.ts
+++ src/shared/utils/utils.ts
@@ -1,2 +1,9 @@
+import { REQUEST_STATUS_TO_TYPE_MAP } from "shared/constants";
+import { RequestStatus } from "shared/types";
+
 export const roundToFirstDecimal = (number: number) =>
   Math.round(number * 10) / 10;
+
+export const getStyleTypeFromStatus = (status: RequestStatus) => {
+  return REQUEST_STATUS_TO_TYPE_MAP[status] || "default";
+};

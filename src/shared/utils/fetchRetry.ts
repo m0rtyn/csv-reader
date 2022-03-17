4fa2ae45874b0bb96d05d15708259f6347335c95
Martyn <zogacc@gmail.com>
2022-03-17 12:38:56 +0200

    fix: fetch repeater

--- src/shared/utils/fetchRetry.ts
+++ src/shared/utils/fetchRetry.ts
@@ -4,9 +4,17 @@ export const fetchRetry = async (
   count: number
 ): Promise<Response> => {
   try {
-    return await fetch(url, options);
+    const response = await fetch(url, options).then((res) => {
+      if (!res.ok) {
+        throw new Error("Request failure");
+      }
+      return res.json();
+    });
+
+    return response;
   } catch (err) {
     if (count === 1) throw err;
+
     return await fetchRetry(url, options, count - 1);
   }
 };
